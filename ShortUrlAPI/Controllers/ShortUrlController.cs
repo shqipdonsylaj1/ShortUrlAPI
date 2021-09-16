c065929c9b0efe600de2e0c10467ab3aa92d920f
shqipdonsylaj1 <shqipdonsylaj1@gmail.com>
2021-09-17 01:40:14 +0200

    First Commit

---
+++ ShortUrlAPI/Controllers/ShortUrlController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShortUrlAPI.Model;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ShortUrlAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShortUrlController : ControllerBase
+    {
+        #region Properties
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region publicMethod
+        [HttpPost("/short")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public Task Create_Short_Url(string url)
+        {
+            HttpContext context = HttpContext;
+            if (!Uri.TryCreate(url, UriKind.Absolute, out Uri result))
+            {
+                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                return context.Response.WriteAsync("Could not understand URL.");
+            }
+            var results = result.ToString();
+            var entry = new ShortLink
+            {
+                urlId = 123_456_789,
+                longUrl = results
+            };
+            var longsUrl = entry.getLongUrl();
+            var responseUri = $"{context.Request.Scheme}://{context.Request.Host}/{longsUrl}";
+            return context.Response.WriteAsync(responseUri);
+        }
+        [HttpGet("/redirectUrl")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public Task Redirect_Url(string url)
+        {
+            HttpContext context = HttpContext;
+            var entry = new ShortLink
+            {
+                urlId = 123_456_789,
+                longUrl = url
+            };
+            var shortUrl = entry.getLongUrl();
+            var path = context.Request.Path.ToUriComponent().Trim('/');
+            // Call method for find Id in db...
+            var id = ShortLink.GetId(shortUrl);
+            if (entry != null)
+                //context.Response.Redirect(entry.longUrl);
+                return context.Response.WriteAsync(entry.longUrl);
+            else
+                context.Response.Redirect("/");
+            return Task.CompletedTask;
+        }
+        #endregion
+
+        #region privateMethod
+
+        #endregion
+    }
+}
---
+++ ShortUrlAPI/Model/ShortLink.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShortUrlAPI.Model
+{
+    public class ShortLink
+    {
+        #region Properties
+        public int urlId { get; set; }
+        public string longUrl { get; set; }
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region publicMethod
+        public string getLongUrl()
+        {
+            return WebEncoders.Base64UrlEncode(BitConverter.GetBytes(urlId));
+        }
+        public static int GetId(string longUrl)
+        {
+            return BitConverter.ToInt32(WebEncoders.Base64UrlDecode(longUrl));
+        }
+        #endregion
+
+        #region privateMethod
+
+        #endregion
+    }
+}
